3a182afaab8a359a1e273180b8c8303fa9a532e5
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-09-26 18:46:30 +0600

    remind it runs on coroutine not the function,
    and it does not get called untill await is called

---
+++ 001_SE_prep/asyncio/chapter_3_coroutines.py
@@ -0,0 +1,43 @@
+import asyncio
+from datetime import datetime
+
+
+def print_now():
+    print(datetime.now())
+
+
+async def keep_printing(name: str = "") -> None:
+    while True:
+        print(name, end=" ")
+        print_now()
+        await asyncio.sleep(0.5)
+
+
+async def async_main() -> None:
+    try:
+        await asyncio.wait_for(
+            asyncio.gather(
+                keep_printing("First"), keep_printing("Second"), keep_printing("Third")
+            ),
+            5,
+        )
+    except asyncio.TimeoutError:
+        print("Oops, time's up!")
+
+
+async def async_main_wrong() -> None:
+    # these three call are essentially synchrounous
+    # though it will help if those functions are io bound, leaving
+    # this process and work for ther job
+    try:
+        await keep_printing("First")
+        await keep_printing("Second")
+        await keep_printing("Third")
+
+    except asyncio.TimeoutError:
+        print("Oops, time's up!")
+
+
+if __name__ == "__main__":
+    # asyncio.run(async_main())
+    asyncio.run(async_main_wrong())
