dbd3d11fc250aa92b5530eb57cb670550ebb736e
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-10-01 08:16:31 +0600

    await callback

---
+++ 001_SE_prep/asyncio/chapter_4_example_slicing.py
@@ -0,0 +1,39 @@
+import asyncio
+
+
+async def example(count: int) -> str:
+    print("Before the first wait")
+    await asyncio.sleep(0)
+
+    if count == 0:
+        print("Returning result")
+        return "result"
+
+    for i in range(count):
+        print(f"Before await inside the loop iteration {i}")
+        await asyncio.sleep(i)
+        print(f"After await inside the loop iteration {i}")
+
+    print(f"Before await on example {count - 1}")
+
+    return await example(count - 1)
+
+
+class TraceStep(asyncio.tasks._PyTask):
+    def _Task__step(self, exc=None):
+        print(f"<step(^) name={self.get_name()} done={self.done()}>")
+
+        _ = super()._Task__step(exc=exc)
+        print(f"</step name={self.get_name()} done={self.done()}>")
+
+
+if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.set_task_factory(lambda loop, coro: TraceStep(coro, loop=loop))
+
+    try:
+        result = loop.run_until_complete(example(2))
+        print("Final result:", result)
+    finally:
+        loop.close()
